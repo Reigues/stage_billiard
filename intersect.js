85039bc16b84adabae8fa8f9777bc04c2159ff03
Reigues <sangoi.remy@gmail.com>
2022-05-18 21:45:35 +0200

    day2 night

---
+++ intersect.js
@@ -0,0 +1,82 @@
+// Javascript program to check if two given line segments intersect
+ 
+
+// Given three collinear points p, q, r, the function checks if
+// point q lies on line segment 'pr'
+function onSegment(p, q, r)
+{
+    if (q.x <= Math.max(p.x, r.x) && q.x >= Math.min(p.x, r.x) &&
+        q.y <= Math.max(p.y, r.y) && q.y >= Math.min(p.y, r.y))
+    return true;
+   
+    return false;
+}
+ 
+// To find orientation of ordered triplet (p, q, r).
+// The function returns following values
+// 0 --> p, q and r are collinear
+// 1 --> Clockwise
+// 2 --> Counterclockwise
+function orientation(p, q, r)
+{
+ 
+    // See https://www.geeksforgeeks.org/orientation-3-ordered-points/
+    // for details of below formula.
+    let val = (q.y - p.y) * (r.x - q.x) -
+            (q.x - p.x) * (r.y - q.y);
+   
+    if (val == 0) return 0; // collinear
+   
+    return (val > 0)? 1: 2; // clock or counterclock wise
+}
+ 
+// The main function that returns true if line segment 'p1q1'
+// and 'p2q2' intersect.
+function doIntersect(line1, line2)
+{
+    var [p1, q1, p2, q2] = [line1.u, line1.v, line2.u, line2.v]
+ 
+    // Find the four orientations needed for general and
+    // special cases
+    let o1 = orientation(p1, q1, p2);
+    let o2 = orientation(p1, q1, q2);
+    let o3 = orientation(p2, q2, p1);
+    let o4 = orientation(p2, q2, q1);
+   
+    // General case
+    if (o1 != o2 && o3 != o4)
+        return true;
+   
+    // Special Cases
+    // p1, q1 and p2 are collinear and p2 lies on segment p1q1
+    if (o1 == 0 && onSegment(p1, p2, q1)) return true;
+   
+    // p1, q1 and q2 are collinear and q2 lies on segment p1q1
+    if (o2 == 0 && onSegment(p1, q2, q1)) return true;
+   
+    // p2, q2 and p1 are collinear and p1 lies on segment p2q2
+    if (o3 == 0 && onSegment(p2, p1, q2)) return true;
+   
+    // p2, q2 and q1 are collinear and q1 lies on segment p2q2
+    if (o4 == 0 && onSegment(p2, q1, q2)) return true;
+   
+    return false; // Doesn't fall in any of the above cases
+}
+
+
+function line_intersect(line1, line2)
+{
+    var [x1, y1, x2, y2, x3, y3, x4, y4] = [line1.u.x, line1.u.y, line1.v.x, line1.v.y, line2.u.x, line2.u.y, line2.v.x, line2.v.y]
+    var ua, ub, denom = (y4 - y3)*(x2 - x1) - (x4 - x3)*(y2 - y1);
+    if (denom == 0) {
+        return null;
+    }
+    ua = ((x4 - x3)*(y1 - y3) - (y4 - y3)*(x1 - x3))/denom;
+    ub = ((x2 - x1)*(y1 - y3) - (y2 - y1)*(x1 - x3))/denom;
+    return {
+        x: x1 + ua * (x2 - x1),
+        y: y1 + ua * (y2 - y1),
+        seg1: ua >= 0 && ua <= 1,
+        seg2: ub >= 0 && ub <= 1
+    };
+}
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let context = canvas.getContext("2d")
 
+let figure=[{x:300,y:400},{x:600,y:400},{x:450,y:400-150*Math.sqrt(3)}]
 let cursor = {
     x: 200,
     y: 200,
@@ -10,14 +11,24 @@ let arrowEnd = {
     r: 100,
     radius: 10
 }
+// let points=new Array(10)
 let offset = {
     top: canvas.offsetTop,
     left: canvas.offsetLeft
 }
 
 function draw(highlight_cursor=false, highlight_arrowEnd=false) {
+
     context.clearRect(0, 0, canvas.width, canvas.height)
     //context.fillRect(cursor.x,cursor.y,cursor.width,cursor.height)
+    context.beginPath();
+    context.moveTo(figure[figure.length-1].x,figure[figure.length-1].y)
+    for (let i = 0; i < figure.length; i++) {
+        context.lineTo(figure[i].x,figure[i].y)
+    }
+    context.lineWidth = 3;
+    context.strokeStyle = 'black';
+    context.stroke();
     context.beginPath();
     context.arc(cursor.x, cursor.y, cursor.radius, 0, 2 * Math.PI, false);
     context.fillStyle="black"
@@ -39,7 +50,32 @@ function draw(highlight_cursor=false, highlight_arrowEnd=false) {
     context.beginPath();
     context.moveTo(cursor.x, cursor.y);
     context.lineTo(cursor.x+1000*Math.cos(arrowEnd.arg), cursor.y+1000*Math.sin(arrowEnd.arg));
+    context.strokeStyle = 'gray';
     context.stroke();
+}
+
+function nextPoint(point,direction) {
+    let line = null
+    for (let i = 0; i < figure.length; i++) {
+        if (i==figure.length-1) {
+            ip1=0
+        }else{
+            ip1=i+1
+        }
+        if(doIntersect({u:figure[i],v:figure[ip1]}, {u:point,v:{x:point.x+1000*Math.cos(direction),y:point.y+1000*Math.sin(direction)}})){
+            line={u:figure[i],v:figure[ip1]}
+        }
+    }
+    /* a1=Math.tan(direction)
+    b1=point.y-a1* */
+    if (line!=null) {
+        let result = line_intersect(line, {u:point,v:{x:point.x+Math.cos(direction),y:point.y+Math.sin(direction)}})
+        console.log(result)
+    }
+}
+
+function points(){
+
 }
 
 draw()
@@ -58,9 +94,11 @@ canvas.onmousemove = function (e) {
     if (posInCursor != null) {
         cursor.x = e.clientX - posInCursor.x - offset.left
         cursor.y = e.clientY - posInCursor.y - offset.top
+        nextPoint(cursor,arrowEnd.arg)
     }
     if (posInArrowEnd != null) {
         arrowEnd.arg = posInArrowEnd.arg + Math.atan2((e.clientY - cursor.y - offset.top),(e.clientX - cursor.x - offset.left))
+        nextPoint(cursor,arrowEnd.arg)
     }
     draw(inCursor, inArrowEnd)
 }
